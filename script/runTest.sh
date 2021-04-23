af1c861356c1c95c16c8c9978ee931497d3ed4e3
Silvio Donato <silvio.donato@cern.ch>
2021-04-23 15:58:17 +0200

    improve runTest

--- script/runTest.sh
+++ script/runTest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NOCHANGED_FILES=""
+ADDED_FILES=""
 CHANGED_FILES=""
 BROKEN_FILES=""
 for i in $(find POG -name *.json); do # Whitespace-safe but not recursive.
@@ -14,19 +15,27 @@ for i in $(find POG -name *.json); do # Whitespace-safe but not recursive.
     else
         DIFF="$(cmp --silent cms-nanoAOD-repo/$i $i; echo $?)"
         if [[ $DIFF -ne 0 ]]; then
-            echo "There are changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
-            echo "-------------- summary - original version -----------------------------------"
-            correction summary cms-nanoAOD-repo/$i
-            echo "-------------- summary - new version ----------------------------------------"
-            correction summary $i
-            echo "-------------- differences in summary ---------------------------------------"
-            correction summary cms-nanoAOD-repo/$i | grep -v "Corrections in file" > tmp1.txt 
-            correction summary $i | grep -v "Corrections in file" > tmp2.txt
-            git diff --no-index tmp1.txt tmp2.txt
-            echo "-------------- differences in file ----------------------------------------"
-            git diff --no-index cms-nanoAOD-repo/$i $i
-            echo "----------------------------------------------------------------------------"
-            CHANGED_FILES=$CHANGED_FILES"\n"$i
+            if test -f "cms-nanoAOD-repo/$i"; then
+                echo "There are changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
+                echo "-------------- summary - original version -----------------------------------"
+                correction summary cms-nanoAOD-repo/$i
+                echo "-------------- summary - new version ----------------------------------------"
+                correction summary $i
+                echo "-------------- differences in summary ---------------------------------------"
+                correction summary cms-nanoAOD-repo/$i | grep -v "Corrections in file" > tmp1.txt 
+                correction summary $i | grep -v "Corrections in file" > tmp2.txt
+                git diff --no-index tmp1.txt tmp2.txt
+                echo "-------------- differences in file ----------------------------------------"
+                git diff --no-index cms-nanoAOD-repo/$i $i
+                echo "----------------------------------------------------------------------------"
+                CHANGED_FILES=$CHANGED_FILES"\n"$i
+            else
+                echo "New file found in "$i
+                echo "-------------- summary of new file -----------------------------------"
+                correction summary $i
+                echo "----------------------------------------------------------------------------"
+                ADDED_FILES=$ADDED_FILES"\n"$i
+            fi
         else
             echo "No changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
             NOCHANGED_FILES=$NOCHANGED_FILES"\n"$i
@@ -35,9 +44,11 @@ for i in $(find POG -name *.json); do # Whitespace-safe but not recursive.
 done
 
 echo
-echo -e "Good files with no changes:"$NOCHANGED_FILES
+echo -e "Files checked:"$NOCHANGED_FILES$CHANGED_FILES$ADDED_FILES$BROKEN_FILES
 echo
-echo -e "Good files with changes:"$CHANGED_FILES
+echo -e "Files changed (tests passed):"$CHANGED_FILES
+echo
+echo -e "Files added (tests passed):"$ADDED_FILES
 echo
 if [[ ${#BROKEN_FILES} -ne 0 ]]; then
     echo -e "Broken files:"$BROKEN_FILES
