ee649f8b1e60254857005f958fcf2431547b1f92
Silvio Donato <silvio.donato@cern.ch>
2021-04-23 14:54:21 +0200

    improve difference in runTest

--- script/runTest.sh
+++ script/runTest.sh
@@ -4,14 +4,17 @@ for i in $(find POG -name *.json); do # Whitespace-safe but not recursive.
     correction validate --version 2 $i
     if [[ $DIFF -ne 0 ]]; then
         echo "There are changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
-        echo "-------------- original version -----------------------------------"
+        echo "-------------- summary - original version -----------------------------------"
         correction summary cms-nanoAOD-repo/$i
-        echo "-------------- new version ----------------------------------------"
+        echo "-------------- summary - new version ----------------------------------------"
         correction summary $i
-        echo "-------------- diff version ----------------------------------------"
-        correction summary cms-nanoAOD-repo/$i > tmp1.txt
-        correction summary $i > tmp2.txt
+        echo "-------------- differences in summary ---------------------------------------"
+        correction summary cms-nanoAOD-repo/$i | grep -v "Corrections in file" > tmp1.txt 
+        correction summary $i | grep -v "Corrections in file" > tmp2.txt
         git diff --no-index tmp1.txt tmp2.txt
+        echo "-------------- differences in file ----------------------------------------"
+        git diff --no-index cms-nanoAOD-repo/$i $i
+        echo "----------------------------------------------------------------------------"
     else
         echo "No changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
     fi
