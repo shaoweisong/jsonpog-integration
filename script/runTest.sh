b3c85695c5e9d098b1712fe6652fabdec5311d34
Silvio Donato <silvio.donato@cern.ch>
2021-04-23 14:45:43 +0200

    fix in script/runTest.sh

--- script/runTest.sh
+++ script/runTest.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 for i in $(find POG -name *.json); do # Whitespace-safe but not recursive.
     DIFF="$(cmp --silent cms-nanoAOD-repo/$i $i; echo $?)"
-    correction validate --version 2
+    correction validate --version 2 $i
     if [[ $DIFF -ne 0 ]]; then
         echo "There are changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
         echo "-------------- original version -----------------------------------"
@@ -12,5 +13,6 @@ for i in $(find POG -name *.json); do # Whitespace-safe but not recursive.
         correction summary $i > tmp2.txt
         git diff --no-index tmp1.txt tmp2.txt
     else
-        echo "There is no changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
+        echo "No changes in "$i" wrt cms-nanoAOD/jsonpog-integration.git. "
     fi
+done
