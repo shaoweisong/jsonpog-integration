d21ccf21a3b13a9554afce15391ff77072546a85
Xavier Coubez <xavier.coubez@cern.ch>
2021-09-01 17:06:31 +0200

    Adding UL 18 and sanity check

--- examples/btvExample.py
+++ examples/btvExample.py
@@ -22,4 +22,18 @@ valsf_deepJet_shape = evaluator["deepJet_106XUL17SF_shape"].evaluate("central", 
 print("deepJet reshaping scale factor is: "+str(valsf_deepJet_shape))
 
 
+# Sanity check of the UL 18 version
 
+evaluator = _core.CorrectionSet.from_file('./../POG/BTV/2018_UL/bjets.json')
+
+valsf_deepCSV_wp = evaluator["deepCSV_106XUL18SF_wp"].evaluate("central", "comb", 0, 0, 0., 30.)
+print("deepCSV working point scale factor in UL 18 is: "+str(valsf_deepCSV_wp))
+
+valsf_deepCSV_shape = evaluator["deepCSV_106XUL18SF_shape"].evaluate("central", 2, 0., 20., 0.8)
+print("deepCSV reshaping scale factor in UL 18 is: "+str(valsf_deepCSV_shape))
+
+valsf_deepJet_wp = evaluator["deepJet_106XUL18SF_wp"].evaluate("central", "comb", 0, 0, 0., 30.)
+print("deepJet working point scale factor in UL 18 is:  "+str(valsf_deepJet_wp))
+
+valsf_deepJet_shape = evaluator["deepJet_106XUL18SF_shape"].evaluate("central", 2, 0., 20., 0.8)
+print("deepJet reshaping scale factor in UL 18 is: "+str(valsf_deepJet_shape))
