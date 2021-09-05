eb98436e6e12a2b82a0eb6b1bc4be65019c62e91
Loukas Gouskos <Loukas.Gouskos@cern.ch>
2021-09-05 17:09:55 +0200

    small tweaks

--- script/createHtmlJsonFiles.sh
+++ script/createHtmlJsonFiles.sh
@@ -2,6 +2,7 @@
 #pogs=("BTV" "JME" "EGM" "MUO" "TAU")
 pogs=("BTV" "EGM" "MUO" "TAU")
 eras=("2016preVFP" "2016postVFP" "2017" "2018")
+period="Run2"
 campaign="UL"
 
 for pog in "${pogs[@]}";
@@ -17,10 +18,11 @@ do
 
 	for jsonname in "${jsonnames[@]}";
 	do
-	    correction --html ${pog}_${jsonname}_${era}_${campaign}.html summary jsonpog-integration/POG/${pog}/${era}_${campaign}/${jsonname}.json
-	    dir="/Volumes/dfs/Websites/c/cms-nanoAOD-integration/commonJSONSFs/"${pog}_${jsonname}_${era}_${campaign}
+	    correction --html ${pog}_${jsonname}_${era}_${campaign}.html summary ../../jsonpog-integration/POG/${pog}/${era}_${campaign}/${jsonname}.json
+	    dir="/Volumes/dfs/Websites/c/cms-nanoAOD-integration/commonJSONSFs/"${pog}_${jsonname}_${period}_${campaign}
 	    if [ ! -d "${dir}" ]; then mkdir ${dir}; fi
 	    mv ${pog}_${jsonname}_${era}_${campaign}.html ${dir}
+	    rm ${dir}/._*
 	done
 	
     done
