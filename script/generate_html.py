880df502a48810f003c25ba455cb09a2c4c311ed
Sebastien Wertz <sebastien.wertz@cern.ch>
2022-12-14 11:50:09 +0100

    adjustments to scripts

--- script/generate_html.py
+++ script/generate_html.py
@@ -10,14 +10,16 @@ from rich.console import Console
 from correctionlib.highlevel import model_auto, open_auto
 
 
-template = """
-<html>
+template = \
+"""<html>
 <head>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery.fancytable/dist/fancyTable.min.js"></script>
+    <script src="fancyTable.min.js"></script>
+
+    <h3>Summary of common POG JSONs</h3>
     
     #TABLE#
 
@@ -27,7 +29,7 @@ template = """
                 sortColumn: 0,
                 pagination: true,
                 exactMatch: "auto",
-                perPage: 50
+                perPage: 25
             });
         });
     </script>
@@ -63,45 +65,45 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", required=True, help="output folder for html pages")
     args = parser.parse_args()
 
-    outPath = pl.Path(args.output)
-    outPath.mkdir(parents=True, exist_ok=True)
+    out_dir_p = pl.Path(args.output)
+    summary_dir_p = out_dir_p / "summaries"
+    summary_dir_p.mkdir(parents=True, exist_ok=True)
 
-    rootPath = pl.Path(args.input)
+    root_dir_p = pl.Path(args.input)
     files = []
     
-    for pogDir in rootPath.iterdir():
-        if not pogDir.is_dir(): continue
-        pog = pogDir.name
+    for pog_dir_p in root_dir_p.iterdir():
+        if not pog_dir_p.is_dir(): continue
+        pog = pog_dir_p.name
         
-        for eraDir in pogDir.iterdir():
-            if not eraDir.is_dir(): continue
+        for era_dir_p in pog_dir_p.iterdir():
+            if not era_dir_p.is_dir(): continue
 
-            era = eraDir.name
+            era = era_dir_p.name
             year, campaign, run = get_run_from_era(era)
 
-            for fileH in eraDir.iterdir():
-                if fileH.suffixes not in [[".json"], [".json", ".gz"]]: continue
-                fileNm = fileH.name.split(".")[0]
+            for json_file_p in era_dir_p.iterdir():
+                if json_file_p.suffixes not in [[".json"], [".json", ".gz"]]: continue
+                file_name = json_file_p.name.split(".")[0]
 
-                summary_path = outPath / f"{pog}_{era}_{fileNm}.html"
-                print(f"Generating HTML summary for {fileH}")
-                generate_json_summary(str(fileH), str(summary_path))
+                summary_file_p = summary_dir_p / f"{pog}_{era}_{fileNm}.html"
+                print(f"Generating HTML summary for {file_p}")
+                generate_json_summary(json_file_p, summary_file_p)
 
                 files.append({
                     "POG": pog,
-                    "Era": era,
-                    "Year": year,
+                    "Era": year,
                     "Campaign": campaign,
-                    "Run": run,
-                    "File": f'<a href="{summary_path.name}">{fileNm}</a>'
+                    "LHC Run": run,
+                    "File summary": f'<a href="summaries/{summary_file_p.name}">{file_name}</a>'
                 })
 
     print("Generating index page")
 
     files = pd.DataFrame(files)
 
-    outIndex = outPath / "index.html"
-    outIndex.write_text(
+    index_p = out_dir_p / "index.html"
+    index_p.write_text(
         template.replace("#TABLE#",
                          files.to_html(index=False, table_id="jsonTable", escape=False)
                         )
